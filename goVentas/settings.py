b61891af20f808accc598921f389e2f969e62054
Pedro Ortiz <portiz130@gmail.com>
2024-08-14 11:24:03 -0600

    Feat: Nueva ip para la base de datos...

--- goVentas/settings.py
+++ goVentas/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-+we5g3*bxceyp_or@&uyefe@z8%m!avpxkc5$(dhgh2xqs=cr5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','3.92.178.87','172.31.85.182']
 
 
 # Application definition
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'djangodb', 
         'USER':'djangodbuser',
         'PASSWORD':'64lgDAdr28NbvED%zI1wE$9*oAc3Sgqn', 
-        'HOST': 'localhost', 
+        'HOST': '172.31.88.212', 
         'PORT': '5432',
     },
     'clients':{
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': 'gocontroldb', 
         'USER':'gocontroluserdb',
         'PASSWORD':'GBInf^r51Jz7gXtIMntfcoM9r03lL#ET', 
-        'HOST': 'localhost', 
+        'HOST': '172.31.88.212', 
         'PORT': '5432',
     }
 }
